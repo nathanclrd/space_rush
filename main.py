88a6557fe2090fe07b5d3d011823b017a1bd8da9
nathanclrd <colardnathan1803@gmail.com>
2024-11-05 08:55:56 +0100

    .

--- main.py
+++ main.py
@@ -1,116 +1,141 @@
 import pygame
-import random
+
 pygame.init()
 
+image_fond = pygame.image.load("./assets/map.png")
+dimensions_fenetre = (800, 600)
+LARGEUR_FENETRE, HAUTEUR_FENETRE = dimensions_fenetre[0], dimensions_fenetre[1]
 
-bg_image = pygame.image.load("./assets/map.png")
-dimensions_fenetre = (800,600)
-LARGEUR_FENETRE,HAUTEUR_FENETRE= dimensions_fenetre[0],dimensions_fenetre[1]
+ecran = pygame.display.set_mode(dimensions_fenetre)
 
-screen = pygame.display.set_mode(dimensions_fenetre)
+police = pygame.font.SysFont('monospace', HAUTEUR_FENETRE // 20, True)
+ROUGE = (255, 0, 0)
 
-police = pygame.font.SysFont('monospace', HAUTEUR_FENETRE//20, True)
-JAUNE= (255,255,0)
+son_limite = pygame.mixer.Sound("./assets/beep.mp3")
+projectile_tire = False
 
-limit_sound = pygame.mixer.Sound("./assets/limit.mp3")
+horloge = pygame.time.Clock()
 
+OFFSET_BAS = 40
 
-BOTTOM_OFFSET = 40
-
-gameIcon = pygame.image.load("./assets/icon.png")
-
+icone_jeu = pygame.image.load("./assets/icon.png")
 pygame.display.set_caption("Space Invaders")
- 
-# set icon
-pygame.display.set_icon(gameIcon)
+pygame.display.set_icon(icone_jeu)
 
+def afficher_fond():
+    taille_fenetre = ecran.get_size()
+    image_redimensionnee = pygame.transform.smoothscale(image_fond, taille_fenetre)
+    ecran.blit(image_redimensionnee, (0, 0))
 
-def bg():
-    resized = screen.get_size()
-    scaled_image = pygame.transform.smoothscale(bg_image,resized)
-    screen.blit(scaled_image,(0,0))
-    
-    
 joueur = {
-    "image" : pygame.transform.smoothscale(pygame.image.load("./assets/SpaceShip.png").convert_alpha(),(85,85)),
-    "position" : [0,0],
-    "vitesse" : [0,0],
-    "acceleration": [0,0],
-    "vie" : 100,
+    "image": pygame.transform.smoothscale(pygame.image.load("./assets/SpaceShip.png").convert_alpha(), (85, 85)),
+    "position": [0, 0],
+    "vitesse": [0, 0],
+    "acceleration": [0, 0],
+    "vie": 100,
 }
-joueur["size"] = joueur["image"].get_size()
-joueur["position_init"] = [LARGEUR_FENETRE//2-joueur['size'][0]//2,HAUTEUR_FENETRE-joueur['size'][1]-BOTTOM_OFFSET]
+joueur["taille"] = joueur["image"].get_size()
+joueur["position_initiale"] = [LARGEUR_FENETRE // 2 - joueur['taille'][0] // 2, HAUTEUR_FENETRE - joueur['taille'][1] - OFFSET_BAS]
 
-message = police.render("Vous avez atteint la limite du monde", True,JAUNE)
+projectile = {
+    "image": pygame.transform.smoothscale(pygame.image.load("./assets/Projectile.png").convert_alpha(), (20, 30)),
+    "position": [0, 0],
+    "vitesse": [0, 8],
+    "acceleration": [0, 0],
+}
+projectile["taille"] = projectile["image"].get_size()
+projectile["position_initiale"] = [LARGEUR_FENETRE // 2 - projectile['taille'][0] // 2, HAUTEUR_FENETRE - projectile['taille'][1] - OFFSET_BAS]
+
+message = police.render("Vous avez atteint la limite du monde", True, ROUGE)
 message_largeur, message_hauteur = police.size("Vous avez atteint la limite du monde")
-message_position = ((LARGEUR_FENETRE - message_largeur) // 2, HAUTEUR_FENETRE // 3)
+position_message = ((LARGEUR_FENETRE - message_largeur) // 2, HAUTEUR_FENETRE // 3)
 
-show_text = True
-blink_interval = 0.5
-last_blink_time = pygame.time.get_ticks()/1000
+afficher_texte = True
+intervalle_clignotement = 0.5
+dernier_temps_clignotement = pygame.time.get_ticks() / 1000
+temps_actuel = pygame.time.get_ticks() / 1000
+dernier_temps = 0
 
-def newEnnemy():
+def nouvel_ennemi(id):
     return {
-    "position" : [0,0],
-    "vitesse" : [0,0],
-    "acceleration": [0,0],
-    "vie" : 100,
-}
+        "id": id,
+        "position": [0, 0],
+        "vitesse": [0, 0],
+        "acceleration": [0, 0],
+        "vie": 100,
+    }
 
+liste_ennemis = [nouvel_ennemi(x) for x in range(10)]
+print(liste_ennemis)
 
-def inputs():
-    global running,joueur
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            #controls
-        if event.type == pygame.KEYDOWN:      
-            if event.key == pygame.K_RIGHT:
-                joueur['position'][0] += 12
-            if event.key == pygame.K_LEFT:
-                joueur['position'][0] -= 12
-            if event.key == pygame.K_UP:
-                joueur['position'][1] -=12
-            if event.key == pygame.K_DOWN:
-                joueur['position'][1] +=12
+dt = temps_actuel - dernier_temps
 
-joueur['position'] = joueur['position_init']
-def displayPlayer():
-    screen.blit(joueur['image'],(joueur['position']))
+def gerer_entrees():
+    global en_cours, joueur, projectile, projectile_tire, dernier_temps, dt
+    for evenement in pygame.event.get():
+        if evenement.type == pygame.QUIT:
+            en_cours = False
 
-def collision():
-    global HAUTEUR_FENETRE,LARGEUR_FENETRE
-    if joueur['position'][0]+joueur['size'][0] >= LARGEUR_FENETRE:
-        joueur['position'][0] = LARGEUR_FENETRE - joueur['size'][0]
-    if joueur['position'][0]<= 0:
+    touches = pygame.key.get_pressed()
+    if touches[pygame.K_RIGHT]:
+        joueur['position'][0] += 12
+    if touches[pygame.K_LEFT]:
+        joueur['position'][0] -= 12
+    if touches[pygame.K_UP]:
+        joueur['position'][1] -= 12
+    if touches[pygame.K_DOWN]:
+        joueur['position'][1] += 12
+    if touches[pygame.K_SPACE] and not projectile_tire:
+        projectile['position'][1] = joueur['position'][1]
+        projectile['position'][0] = joueur['position'][0] + (joueur['taille'][0] // 2) - (projectile['taille'][0] // 2)
+        projectile_tire = True
+
+joueur['position'] = joueur['position_initiale']
+
+def afficher_joueur():
+    ecran.blit(joueur['image'], (joueur['position']))
+
+def detecter_collisions():
+    if joueur['position'][0] + joueur['taille'][0] >= LARGEUR_FENETRE:
+        joueur['position'][0] = LARGEUR_FENETRE - joueur['taille'][0]
+    if joueur['position'][0] <= 0:
         joueur['position'][0] = 0
-    if joueur['position'][1]+joueur['size'][1]>= HAUTEUR_FENETRE:
-        joueur['position'][1] = HAUTEUR_FENETRE - joueur['size'][1]
-    if joueur['position'][1]<= -200:
+    if joueur['position'][1] + joueur['taille'][1] >= HAUTEUR_FENETRE:
+        joueur['position'][1] = HAUTEUR_FENETRE - joueur['taille'][1]
+    if joueur['position'][1] <= -200:
         joueur['position'][1] = -200
-        flicker_text(message,message_position)
-        
-        
-        ## Commencer a perdre la barre de vie si on reste
+        clignoter_texte(message, position_message)
 
-def flicker_text(message,position):
-    global last_blink_time, blink_interval, show_text
-    if pygame.time.get_ticks()/1000 - last_blink_time > blink_interval:
-        show_text = not show_text  # Alterner l'affichage du texte
-        last_blink_time = pygame.time.get_ticks()/1000  # Réinitialiser le temps
-    if show_text :
-        screen.blit(message,position)
-        pygame.mixer.Sound.play(limit_sound)
+def clignoter_texte(texte, position):
+    global dernier_temps_clignotement, intervalle_clignotement, afficher_texte
+    if pygame.time.get_ticks() / 1000 - dernier_temps_clignotement > intervalle_clignotement:
+        afficher_texte = not afficher_texte
+        dernier_temps_clignotement = pygame.time.get_ticks() / 1000
+    if afficher_texte:
+        ecran.blit(texte, position)
+        pygame.mixer.Sound.play(son_limite)
 
+projectile['position'] = projectile['position_initiale']
+
+def tirer_projectile():
+    global projectile, projectile_tire
+    if projectile_tire:
+        projectile['position'][1] -= projectile['vitesse'][1]
+        ecran.blit(projectile['image'], (projectile['position']))
+    if projectile['position'][1] < 0:
+        projectile_tire = False
 
 pygame.key.set_repeat(40, 20)
-running = True
-while running:
-    bg()
-    inputs()
-    collision()
-    displayPlayer()
+en_cours = True
+while en_cours:
+    temps_actuel = pygame.time.get_ticks() / 1000
+    dt = temps_actuel - dernier_temps
+    afficher_fond()
+    gerer_entrees()
+    detecter_collisions()
+    afficher_joueur()
+    tirer_projectile()
     pygame.display.flip()
-            
-            
+    horloge.tick(60)
+
 pygame.quit()
